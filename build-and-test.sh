974fe4cf25de1904c0db7ecf3e538d4ca951c00f
ferris <yupferris@gmail.com>
2019-11-12 18:33:13 +0100

    Add ch5 seq

--- build-and-test.sh
+++ build-and-test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cargo build --release && (cp ./target/release/opengl32.dll ./test/Mosaik/; pushd test/Mosaik/; ./Mosaik; popd)
+cargo build --release && (cp ./target/release/opengl32.dll ./test/hjb_ch5s/; pushd test/hjb_ch5s/; ./ch5seq; popd)
